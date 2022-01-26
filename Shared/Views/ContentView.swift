4b433b8bf2c13ba0f7e192cec4af45d0f9ee6959
Abdul Khan <akhan@lcs.on.ca>
2022-01-26 17:22:45 -0500

    Debugged: Saving a new task did not display it in the list, now it does.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -95,7 +95,7 @@ struct ContentView: View {
             }
         }
         .sheet(isPresented: $showingAddTask) {
-            AddTask(store: testStore, showing: $showingAddTask)
+            AddTask(store: store, showing: $showingAddTask)
         }
     }
 }
